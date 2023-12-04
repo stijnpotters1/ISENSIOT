0e205ddb54d0f41a6c91feb5b521213f0b1d05ff
Stijn Potters <s1137088@student.hsleiden.nl>
2023-12-04 20:30:45 +0100

    feat: initial commit

---
+++ glove.ino
@@ -0,0 +1,11 @@
+// initial glove
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ robot-arm.ino
@@ -0,0 +1,11 @@
+// initial robot arm file
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
